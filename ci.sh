1ffd9f811be83a75647fea7186d7d0ac1143473c
Chris Bacon <chrisbacon2009@hotmail.com>
2019-01-25 18:40:21 +0000

    Added cleanup and output on green tests

--- ci.sh
+++ ci.sh
@@ -1,13 +1,14 @@
 git clone https://github.com/exosphere-lang/examples.git
 git clone https://github.com/exosphere-lang/compiler.git
 
-cd compiler
-
-stack build --copy-bins
-
+cd compiler && stack build --copy-bins
 cd ..
 
 for i in $(find ./ -name \*.exo); do
     OUTPUT=`exospherec $i`
     [ ! -z "$OUTPUT" ] && echo "ERR: compiling $i: $OUTPUT" && exit 1
 done
+
+echo "All examples successfully compiled..."
+
+rm -rf examples compiler
